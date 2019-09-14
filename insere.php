ff3cb75489bbded172f47927ea046819dc76f38a
Narciso <narcisoo1@hotmail.com>
2019-09-14 15:04:49 -0300

    MUDA TUDO

--- insere.php
+++ insere.php
@@ -3,12 +3,17 @@ include 'conexao.php';
 $nome = $_POST['nome'];
 $sobrenome = $_POST['sobrenome'];
 $email = $_POST['email'];
+$c_senha = $_POST['c_senha'];
 $senha = $_POST['senha'];
+$telefone = $_POST['telefone'];
+$data_nascimento = $_POST['data_nascimento'];
+$genero = $_POST['genero'];
 
-$sql = "INSERT INTO Cliente (nome,sobrenome,email,senha) VALUES ('$nome','$sobrenome','$email','$senha')";
+
+$sql = "INSERT INTO Cliente (nome,sobrenome,email,senha,data_nascimento,telefone,genero) VALUES ('$nome','$sobrenome','$email','$senha','$data_nascimento','$telefone','$genero')";
 
 if($conexao->query($sql)=== TRUE){
-    echo "USER CADASTRADO!";
+    header('Location: index.html');
 }else{
     echo "ERRO: ".$sql."<br>".$conexao->error;
 }
